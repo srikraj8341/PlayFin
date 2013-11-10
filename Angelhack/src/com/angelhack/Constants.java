8d52ae29a159c1cf832348358d157d7e3d55f55e
srikanth ayalasomayajulu <srikanth@phabletlabs.com>
2013-11-10 10:21:58 +0530

    angelhack_playfin
    start playing with new individuals with similar interests in our
    location

---
+++ Angelhack/src/com/angelhack/Constants.java
@@ -0,0 +1,132 @@
+package com.angelhack;
+
+public class Constants {
+
+	public static boolean IS_ACTIVE = false;
+
+	public static final String INTERNET_MODE_WIFI = "WIFI";
+	public static final String INTERNET_MODE_MOBILE = "MOBILE";
+	public final static String SendMessage = "SendMessage";
+	public final static String GetChatMessages = "GetChatMessages";
+	public final static int USER_TYPE = 1;
+	public static final String NO_INTERNET_MESSAGE = "Internet Connection Not Available";
+	public final static String URL = "http://bravemount.com/dogalize/services/service.asmx";
+	public final static String ATTACHMENT_URL = "http://bravemount.com/dogalize/services/UploadFile.aspx";
+	public final static String NO_INTERNET_TITLE = "Oops!";
+	public final static String TERMS_CONDITIONS_ALERT = "In order to use our services, you must agree to Dogalize Terms of Service and Privacy Policy";
+
+	public final static String NAMESPACE = "http://vm.org/";
+	public final static int SIGNUPTYPE = 1;
+	public final static String SOCIALID = "";
+	public final static long CREATEDON = 123456;
+
+	// public final static String URL =
+	// "http://192.168.2.17:5245/Services/Service.asmx";
+	public final static long MAX_ATTACHMENT_SIZE = 5242880;
+	// public final static String FIX_APP_CUSTOMER_LINK_ANDROID =
+	// "Get Fixapp now on android https://play.google.com/store/apps/details?id=com.fixapp.customer";
+	// public final static String FIX_APP_SP_LINK_ANDROID =
+	// "Get Fixapp now on android https://play.google.com/store/apps/details?id=com.fixapp.sp";
+	//
+	// public final static String FIX_APP_CUSTOMER_LINK_IPHONE =
+	// "Get Fixapp now on iPhone https://itunes.apple.com/us/app/fixapp-customer/id665821649?ls=1&mt=8";
+	// public final static String FIX_APP_SP_LINK_IPHONE =
+	// "Get Fixapp now on iPhone https://itunes.apple.com/us/app/fixapp-service-provider/id665823201?ls=1&mt=8";
+
+	// public final static String TELL_ME_FRIEND_LINK =
+	// "Hey! Have you tried Fixapp yet?  Find local service providers in your area. Give it a go."
+	// + "\n\n"
+	// + Constants.FIX_APP_CUSTOMER_LINK_ANDROID
+	// + "\n"
+	// + Constants.FIX_APP_CUSTOMER_LINK_IPHONE;;
+
+	public final static String NO_APP_INSTALLED_MESSAGE = "No Application is installed in your device to share link";
+
+	public final static String AUTHENTICATEUSER = "AuthenticateUser";
+	public final static String ISVALIDEMAIL = "IsValidEmail";
+	public final static String CHANGE_PASSWORD = "ChagePassword";
+	public final static String SIGNUP_TYPE = "APP";
+	public final static String SOCILA_SIGNUP_TYPE = "FB";
+	public final static String SEND_VERIFICATION_EMAIL = "SendBackEmailVerification";
+
+	// public final static String FORGOT_PASSWORD = "ForgotPassword";
+	public final static String UPDATE_USER_PROFILE = "UpdateUserProfile";
+	public final static String SIGNUPUSER = "SignUpUser";
+	public final static String SIGNINUSER = "SignInUser";
+	// public final static String GET_STATES = "GetStates";
+	public final static String VERIFICATION_ALERT = "You haven't verified your email address yet. If you want to re-send the verification email to ";
+
+	public final static String UpdateUserProfile = "UpdateUserProfile";
+	public final static String sUpdateUserProfile = "Update User Profile";
+	public final static String AddUserMedia = "AddUserMedia";
+	public final static String sAddUserMedia = "Add User Media";
+	public final static String AddDogMedia = "AddDogMedia";
+	public final static String sAddDogMedia = "Add Dog Media";
+	public final static String GetUserMedia = "GetUserMedia";
+	public final static String sGetUserMedia = "Get User Media";
+	public final static String GetDogMedia = "GetDogMedia";
+	public final static String sGetDogMedia = "Get Dog Media";
+	public final static String DeleteUserMedia = "DeleteUserMedia";
+	public final static String sDeleteUserMedia = "Delete User Media";
+	public final static String DeleteDogMedia = "DeleteDogMedia";
+	public final static String sDeleteDogMedia = "Delete Dog Media";
+	public final static String GetUserDogs = "GetUserDogs";
+	public final static String sGetUserDogs = "Get User Dogs";
+	public final static String CreateDog = "CreateDog";
+	public final static String sCreateDog = "Create Dog";
+	public final static String DeleteDog = "DeleteDog";
+	public final static String sDeleteDog = "Delete Dog";
+	public final static String UpdateDog = "UpdateDog";
+	public final static String sUpdateDog = "Update Dog";
+	public final static String GET_INITIALDATA = "GetInitialData";
+	public final static String GET_NEAR_BY_SPS = "GetNearBySPs";
+	public final static String SOCIAL_SIGNUP_USER = "SocialSignUpUser";
+	public final static String SERVER_NOT_RESPONDING = "Oops something went wrong. Please try again later";
+	public final static String PLAT_FORM = "ANDROID";
+
+	public final static String USER_NAME_PREFERENCE = "USERNAME";
+
+	public final static String USER_LOGIN = "LOGIN";
+	public final static String IS_REMEMBER_ME = "isRemember";
+	public final static String IS_USER_LOG_OUT = "LOGOUT";
+	public final static String IS_FB_SIGNUP = "FB_SIGNUP";
+
+	public final static String LOGIN_EXIT_ALERT_MSG = "Are you sure you want to exit?";
+	public final static String SIGN_OUT_ALERT_MSG = "Are you sure you want to Sign out?";
+	public final static String SIGNUP_EXIT_ALERT_MSG = "Are you sure you want to exit the Sign Up process?";
+	public final static String UPDATE_PROFILE_EXIT_ALERT_MSG = "Are you sure you want to exit the Update Profile process?";
+
+	public final static String EMERGENCY_ALERT_MSG = "Select this if your job is emergency and has to be fixed with in a day";
+	public final static String CATEGORY_EXIT_ALERT_MSG = "Are you sure you want to go cancel this job creation? All data will be lost";
+	public final static String CATEGORY_EDIT_EXIT_ALERT_MSG = "Are you sure you want to go cancel this editing job? All data will be lost";
+	public final static String CLEAR_HISTORY_ALERT_MSG = "Are you sure you want to clear history? This cannot be undone.";
+	public final static String CLEAR_HISTORY_ALERT_TITLE = "Warning!";
+	public final static String NEW_CATEGORY_SUCCESS_MSG = "Your request for a New Category has been Successfully Submitted. We will inform you when it is available";
+
+	public final static String RESPONSE_FEEDBACK = "ResponseFeedBack";
+	public final static int NEW = 0;
+	public final static int READ = 1;
+	public final static int APPLIED = 2;
+	public final static int ACCEPTED = 3;
+	public final static int REJECTED = 4;
+	public final static int COMPLETED = 5;
+	public final static int EXPIRED = 6;
+	public final static int DELETED = 7;
+	public final static int HOLD = 8;
+	public final static int MUTE = 9;
+	public final static int HIDE = 10;
+
+	public final static String NEW_MSG = "NEW";
+	public final static String READ_MSG = "READ";
+	public final static String APPLIED_MSG = "Applied&Waiting";
+	public final static String ACCEPTED_MSG = "Accepted/Assigned";
+	public final static String REJECTED_MSG = "Rejected";
+	public final static String COMPLETED_MSG = "Completed";
+	public final static String EXPIRED_MSG = "Expired";
+	public final static String DELETED_MSG = "Deleted";
+	public final static String HOLD_MSG = "Hold";
+	public final static String MUTE_MSG = "MUTE";
+	public final static String HIDE_MSG = "HIDE";
+	public final static String REPOST_ALERT_MSG = "You can edit the job before reposting it or repost it with the same information as before by pressing 'repost' ";
+
+}
